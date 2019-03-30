fb3188526c0e516bee3c0c5c5b41dfe7220b0126
Aleksandar Pesic <peske.nis@gmail.com>
2019-03-30 21:04:53 +0100

    Initial commit.

---
+++ DeviceRestart/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using DeviceRestartLib;
+
+namespace DeviceRestart
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class MainWindow : Window
+    {
+        #region Static
+
+        private static void Disable()
+        {
+            try
+            {
+                Device.Disable("USB Composite Device", @"USB\VID_1FD2&PID_6103");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Disable failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private static void Enable()
+        {
+            try
+            {
+                Device.Enable("USB Composite Device", @"USB\VID_1FD2&PID_6103");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Enable failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion Static
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Disable_OnClick(object sender, RoutedEventArgs e) => Disable();
+
+        private void Enable_OnClick(object sender, RoutedEventArgs e) => Enable();
+
+        private void ReEnable_OnClick(object sender, RoutedEventArgs e)
+        {
+            Disable();
+            Enable();
+        }
+    }
+}
---
+++ DeviceRestartLib/Device.cs
@@ -0,0 +1,21 @@
+﻿using System.Management.Automation;
+
+namespace DeviceRestartLib
+{
+    public static class Device
+    {
+        private static readonly string GetDeviceFormatString =
+            "Get-PnpDevice | Where-Object {{$_.FriendlyName -eq '{0}' -and $_.InstanceId -like '{1}*' -and $_.Status -ne 'Unknown'}}";
+        private static readonly string DisableDeviceString = " | Disable-PnpDevice -Confirm:$false";
+        private static readonly string EnableDeviceString = " | Enable-PnpDevice -Confirm:$false";
+
+        private static string GetCommand(string friendlyName, string instanceId, bool enable) =>
+            string.Format(GetDeviceFormatString, friendlyName, instanceId) + (enable ? EnableDeviceString : DisableDeviceString);
+
+        public static void Disable(string friendlyName, string instanceId) =>
+            PowerShell.Create().AddScript(GetCommand(friendlyName, instanceId, false)).Invoke();
+
+        public static void Enable(string friendlyName, string instanceId) =>
+            PowerShell.Create().AddScript(GetCommand(friendlyName, instanceId, true)).Invoke();
+    }
+}
